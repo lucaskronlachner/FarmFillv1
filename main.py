27a3b6d19d5a6d9e3ad34ab6e3a56a4d887e02ed
ruttzn <RuttingerE180056@sus.htl-grieskirchen.at>
2022-06-15 12:35:32 +0200

    Formatted and added Gemuese.csv
    poopy stink de la stink poop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
-_filename = "./data/values.csv"
-
 def readcsv():
-    f = open(f"{_filename}", "r")
+    f = open(f"./data/Gemuese.csv", "r")
     for item in f.readlines():
         print(item)
 
