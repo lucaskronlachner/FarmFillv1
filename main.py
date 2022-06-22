a067384df62b0079ad7d00b4aab1cdaa20ca6c5e
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2022-06-22 12:55:12 +0200

    Changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from _vegInfo import _vegInfo
 
 _filename = "./data/values.csv"
-
-
+_years = 4
 _vegetables_info = []
 
 
@@ -10,5 +9,23 @@ def readcsv():
     f = open(f"./data/Gemuese.csv", "r")
     for item in f.readlines():
         print(item)
+
+def createField():
+    _field = []
+    _file = open(f"./data/field.csv", "r")
+    for item in _file.readlines():
+        _splitArr = item.split(';')
+        if len(_field) == 0:
+            for line in _splitArr:
+                line = line.replace('\n', '')
+                _field.append([line])
+        else:
+            counter = 0
+            for line in _splitArr:
+                line = line.replace('\n', '')
+                _field[counter].append(line)
+                counter += 1
+    return _field
+
 
 readcsv()
